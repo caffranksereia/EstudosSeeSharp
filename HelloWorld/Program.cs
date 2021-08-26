dd8b39e9dd0c87d98445976ef5a48476409d6aaa
sr devPotato <circuncisaofabio@icloud.com>
2021-08-26 16:17:34 -0300

    Primeiro quebrar toda a malignidade com o primeiro HelloWorld

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace bankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(args.GetValue(0));
+        }
+    }
+}
